08edcc74888c997b4c543b5991bde1053af334bb
anshu706 <anshupadhayay212@gmail.com>
2025-08-24 14:35:52 +0530

    new code

--- Phase-2/SRDBMS/course.py
+++ Phase-2/SRDBMS/course.py
@@ -2,15 +2,72 @@ from tkinter import *
 from PIL import Image, ImageTk  # pip install pillow
 
 
-class RMS:
+class CourseClass:
     def __init__(self, root):
         self.root = root
         self.root.title("Student Result Management System")
         self.root.geometry("1200x480+80+170")
         self.root.config(bg="white")
+        self.root.focus_force()
+        # == title ==
+        title = Label(self.root, text="Manage Course Details ", font=(
+            "goudy old style", 20, "bold"), bg="#033054", fg="white").place(x=10, y=15, width=1180, height=35)
+
+        # === Variables ===
+        self.var_course = StringVar()
+        self.var_duration = StringVar()
+        self.var_charges = StringVar()
+
+        # === Widgets ===
+        lbl_course = Label(self.root, text="Course Name", font=(
+            "goudy old style", 15, 'bold'), bg='white').place(x=10, y=60)
+        lbl_duration = Label(self.root, text="Duration", font=(
+            "goudy old style", 15, 'bold'), bg='white').place(x=10, y=100)
+        lbl_charges = Label(self.root, text="Charges", font=(
+            "goudy old style", 15, 'bold'), bg='white').place(x=10, y=140)
+        lbl_description = Label(self.root, text="Description", font=(
+            "goudy old style", 15, 'bold'), bg='white').place(x=10, y=180)
+
+#  ==== Entry Fields ====
+        self.txt_courseName = Entry(self.root, textvariable=self.var_course, font=(
+            "goudy old style", 15, 'bold'), bg='white')
+        self.txt_courseName.place(x=150, y=60, width=200)
+
+        self.txt_duration = Entry(self.root, textvariable=self.var_duration, font=(
+            "goudy old style", 15, 'bold'), bg='white')
+        self.txt_duration.place(x=150, y=100, width=200)
+
+        self.txt_charges = Entry(self.root, textvariable=self.var_charges, font=(
+            "goudy old style", 15, 'bold'), bg='white')
+        self.txt_charges.place(x=150, y=140, width=200)
+
+        self.txt_description = Text(self.root, font=(
+            "goudy old style", 15, 'bold'), bg='white')
+        self.txt_description.place(x=150, y=180, width=500, height=130)
+
+        # ==== Buttons ====
+        self.btn_add = Button(self.root, text='Save', font=(
+            "goudy old style", 15, "bold"), bg="#2196f3", fg="white", cursor="hand2")
+        self.btn_add.place(x=150, y=400, width=110, height=40)
+
+        self.btn_update = Button(self.root, text='Update', font=(
+            "goudy old style", 15, "bold"), bg="#4caf50", fg="white", cursor="hand2")
+        self.btn_update.place(x=270, y=400, width=110, height=40)
+
+        self.btn_delete = Button(self.root, text='Delete', font=(
+            "goudy old style", 15, "bold"), bg="#f44336", fg="white", cursor="hand2")
+        self.btn_delete.place(x=390, y=400, width=110, height=40)
+
+        self.btn_clear = Button(self.root, text='Clear', font=(
+            "goudy old style", 15, "bold"), bg="#607d8b", fg="white", cursor="hand2")
+        self.btn_clear.place(x=510, y=400, width=110, height=40)
+
+        # === Search Panel ===
+        lbl_search_courseName = Label(self.root, text="Search By | Course Name", font=(
+            "goudy old style", 15, 'bold'), bg='white').place(x=720, y=60)
 
 
 if __name__ == "__main__":
     root = Tk()
-    obj = RMS(root)
+    obj = CourseClass(root)
     root.mainloop()
--- Phase-2/SRDBMS/dashboard.py
+++ Phase-2/SRDBMS/dashboard.py
@@ -1,5 +1,6 @@
 from tkinter import *
 from PIL import Image, ImageTk  # pip install pillow
+from course import CourseClass
 
 
 class RMS:
@@ -20,22 +21,22 @@ class RMS:
 
 # ----------Button -------------------------------------------------------
         btn_course = Button(M_Frame, text="Course", font=(
-            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2"). place(x=20, y=5, width=200, height=40)
+            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2", command=self.add_course).place(x=20, y=5, width=200, height=40)
 
         btn_student = Button(M_Frame, text="Student", font=(
-            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2"). place(x=240, y=5, width=200, height=40)
+            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=240, y=5, width=200, height=40)
 
         btn_result = Button(M_Frame, text="Result", font=(
-            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2"). place(x=460, y=5, width=200, height=40)
+            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=460, y=5, width=200, height=40)
 
         btn_view = Button(M_Frame, text="View", font=(
-            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2"). place(x=680, y=5, width=200, height=40)
+            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=680, y=5, width=200, height=40)
 
         btn_logout = Button(M_Frame, text="Logout", font=(
-            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2"). place(x=900, y=5, width=200, height=40)
+            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=900, y=5, width=200, height=40)
 
         btn_exit = Button(M_Frame, text="Exit", font=(
-            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2"). place(x=1120, y=5, width=200, height=40)
+            "goudy old style", 15, "bold"), bg="#0b5377", fg="white", cursor="hand2").place(x=1120, y=5, width=200, height=40)
 
 # ==== update_details ====
 
@@ -51,6 +52,10 @@ class RMS:
 # ======Footer ===========
         footer = Label(self.root, text="SRMS - Student Result Management System \n Contact Us for any Techinical Issue: 987xxxxxx9", font=(
             "goudy old style", 12), bg="#262626", fg="white").pack(side=BOTTOM, fill=X)
+
+    def add_course(self):
+        self.new_win = Toplevel(self.root)
+        self.new_obj = CourseClass(self.new_win)
 
 
 if __name__ == "__main__":
